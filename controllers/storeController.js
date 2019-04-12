97f1ee45abd367904122335e614ccf94e8856792
ZazzizzuzA <musicvyp@gmail.com>
2019-04-12 18:43:18 +0300

    kk

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index')
@@ -7,6 +10,9 @@ exports.addStore = (req, res, next) => {
   res.render('editStore', { title: 'Add Store' })
 };
 
-exports.createStore = (req, res, next) => {
-  res.json(req.body)
+exports.createStore = async (req, res, next) => {
+  const store = new Store(req.body);
+  await store.save();
+  console.log('It works!');
+  res.redirect('/');
 }
