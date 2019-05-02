1ecba7f000ddf7dec6a221b96f786839ea6a0023
ZazzizzuzA <musicvyp@gmail.com>
2019-05-02 18:21:02 +0300

    keep going

--- public/dist/App.bundle.js
+++ public/dist/App.bundle.js
@@ -71,7 +71,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* unused harmony export $ */
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return $; });
 /* unused harmony export $$ */
 // based on https://gist.github.com/paulirish/12fb951a8b893a454b32
 var $ = document.querySelector.bind(document);
@@ -106,8 +106,34 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__sass_style_scss__ = __webpack_require__(1);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__sass_style_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__sass_style_scss__);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__modules_bling__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__modules_autocomplete__ = __webpack_require__(4);
 
 
+
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_2__modules_autocomplete__["a" /* default */])(__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__modules_bling__["a" /* $ */])('#address'), __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__modules_bling__["a" /* $ */])('#lat'), __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__modules_bling__["a" /* $ */])('#lng'));
+
+/***/ }),
+/* 3 */,
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+function autocomplete(input, latInput, lngInput) {
+  if (!input) return; // skip this fn from running if there is not input on the page
+
+  var dropdown = new google.maps.places.Autocomplete(input);
+  dropdown.addListener('place_changed', function () {
+    var place = dropdown.getPlace();
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+  }); // if someone hits enter on the address field, don't submit the form
+
+  input.on('keydown', function (e) {
+    if (e.keyCode === 13) e.preventDefault();
+  });
+}
+
+/* harmony default export */ __webpack_exports__["a"] = autocomplete;
 
 /***/ })
 /******/ ]);
